c62530064a33899b85ada6d1dee7101ddf95fe90
Nichuvdr <103339872+Nichuvdr@users.noreply.github.com>
2022-04-14 06:38:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,22 +18,22 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api-alphabot.herokuapp.com/" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.botname = "𝙱𝙾𝚃 𝚅.1" //namabot kalian
+global.ownername= "𝒏𝒊𝒄𝒉𝒖 𝒗𝒅𝒓" //nama kalian
+global.myweb ="https://google.com/" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/alltracker" //bebas asal jan hapus
+global.github = "https://Nichuvdr.github.io/" //bebas
+global.email = "pnichu786@gmail.com" //bebas
+global.region = "India" //bebas
+global.ownernomer = "919207980284" // nomor wa kalian
+global.ownernomerr = "+919207980284" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
-global.sessionName = 'session'
+global.owner = ["919207980284","917994107442","919207980284"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© 𝙱𝙾𝚃 𝚅.1' //sticker wm ubah
+global.author = '𝐵𝑦 𝑁𝑖𝑐ℎ𝑢 𝑣𝑑𝑟' //sticker wm ganti nama kalian
+global.sessionName = '𝐿𝑖𝑣𝑒 𝑛𝑜𝑤'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
