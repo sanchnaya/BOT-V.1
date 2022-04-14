0e7037e0afa32e96b0c55ea9a3899d8029076647
Nichuvdr <103339872+Nichuvdr@users.noreply.github.com>
2022-04-14 17:27:55 +0530

    Update indonesia.js

--- language/indonesia.js
+++ language/indonesia.js
@@ -2,7 +2,7 @@ exports.private = () =>{
 	return`Fitur hanya bisa di gunakan di private chat`
 	}
 exports.wait = () => {
-    return `⏳ Mohon tunggu sebentar~`
+    return `⏳ Please wait...`
 }
 exports.ok = () => {
     return `✅ Done.`
